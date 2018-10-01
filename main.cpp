7a7c45afd1efc121a8192f9846edaee3849b7a9c
Anshima Chaudhary <33005138+anshima1@users.noreply.github.com>
2018-10-01 22:52:19 +0530

    Array and its Reverse
    Program to input elements of an array and display the elements of that array in reverse order.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int main(int argc, char *argv[])
-{
-    cout << "Hello, World!\n";
-
-    return 0;
+// t=number of test cases
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    stack<int> st;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    for(int i=0;i<n;i++){
+	        st.push(a[i]);
+	    }
+	    for(int i=0;i<n;i++){
+	        int x=st.top();
+	        cout<<x<<" ";
+	        st.pop();
+	    }
+	    cout<<endl;
+	}
+	return 0;
 }
